1fad1fa4edefd70d03f60de64412b00be3bf7eaa
Sanyuja Kharwandikar <consience0@gmail.com>
2026-04-05 03:55:34 +0530

    Refactor class_servo.py and remove unused code

--- class_servo.py
+++ class_servo.py
@@ -1,15 +1,18 @@
+#(NOT BEING USED)
+#(this file is not being used)
+
 import serial
 
 class sPort:
     def __init__(self):
-        #ラズパイ4で使うとき
+      
         self.ser_rp4 = serial.Serial('/dev/ttyAMA1', 115200, timeout = None)
     
-    #パケットを送るメソッド
+ 
     def send_packet(self, packet):
         self.ser_rp4.write(packet)
 
-    #シリアルポートを終了する
+ 
     def close_sPort(self):
         self.ser_rp4.close()
 
@@ -17,7 +20,7 @@ class RS304MD:
     def __init__(self, id):
         self.id = id
         self.port = sPort()
-    #トルク状態を変更するメソッド(on:1, off:0, brake:2)
+ 
     def trq_set(self, status):
         id = "00"+str(self.id)
         num = "00"+str(status)
@@ -29,7 +32,6 @@ class RS304MD:
         packet = [int(x, 16) for x in packet]
         self.port.send_packet(packet)
 
-    #サーボを時間指定で目標に動かすメソッド
     def control_time_rotate(self, angle, t):
         a = -angle*10
         if a<0:
@@ -38,12 +40,12 @@ class RS304MD:
         an = []
         an.append('0x'+a[-2:])
         an.append('0x'+a[:2])
-        #時間情報を16進数に変換する
+       
         t = ("0000"+format(int(t*100), 'x'))[-4:]
         tn = []
         tn.append('0x'+t[-2:])
         tn.append('0x'+t[:2])
-        #サーボIDを16進数に変換する
+        
         ID = "00"+str(self.id)
         packet = ['0xFA', '0xAF', '0x' + ID[-2:], '0x00', '0x1E', '0x04', '0x01'] + an + tn 
         sum = 0 
@@ -53,7 +55,7 @@ class RS304MD:
         packet = [int(x, 16) for x in packet]
         self.port.send_packet(packet)
 
-    #サーボを目標位置に動かすメソッド
+   
     def control_rotate(self, angle):
         a = -angle*10
         if a<0:
@@ -62,7 +64,7 @@ class RS304MD:
         an = []
         an.append('0x'+a[-2:])
         an.append('0x'+a[:2])
-        #サーボIDを16進数に変換する
+     
         ID = "00"+str(self.id)
         packet = ['0xFA', '0xAF', '0x' + ID[-2:], '0x00', '0x1E', '0x02', '0x01'] + an 
         sum = 0 
@@ -72,9 +74,8 @@ class RS304MD:
         packet = [int(x, 16) for x in packet]
         self.port.send_packet(packet)
 
-    #サーボ番号を変更する関数
     def change_id(self, new_id):
-        #IDを変更するパケットを作る
+        
         old_id = "00"+str(self.id)
         new_id = "00"+str(new_id)
         packet = ['0xFA', '0xAF', '0x' + old_id[-2:], '0x00', '0x04', '0x01', '0x01', '0x'+new_id[-2:]] 
@@ -85,7 +86,7 @@ class RS304MD:
         packet = [int(x, 16) for x in packet]
         self.port.send_packet(packet)
 
-        #フラッシュROMへの書き込み
+        
         packet = ['0xFA', '0xAF', '0x' + new_id[-2:], '0x40', '0xff', '0x00', '0x00'] 
         sum = 0 
         for num in range(2, len(packet)):
@@ -94,7 +95,7 @@ class RS304MD:
         packet = [int(x, 16) for x in packet]
         self.port.send_packet(packet)
 
-        #サーボを再起動するパケットを作る
+       
         packet = ['0xFA', '0xAF', '0x' + new_id[-2:], '0x20', '0xff', '0x00', '0x00'] 
         sum = 0 
         for num in range(2, len(packet)):
